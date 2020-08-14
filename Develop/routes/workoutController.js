01152e08a9f757b3710f306f5e5a3a7fdb467499
Ha Phan <haphan64@outlook.com>
2020-08-13 23:57:44 -0700

    add routes, server.js, models

---
+++ Develop/routes/workoutController.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const { Workout } = require("../models");
+const { db } = require("../models/WorkoutSchema");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch( err => {
+            res.status(401).json(err);
+        });
+})
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts)
+        })
+        .catch( err => {
+            res.status(401).json(err);
+        })
+})
+
+router.put("/api/workouts/:id", (req, res) => {
+
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body }},
+        { new: true, runValidators: true}               
+    ).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(401).json(err);
+    })
+})
+
+module.exports = router;
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Week17-WorkoutTracker
 Week 17 Homework - Workout Tracker
+Workout Tracker is an app to view, create, and track daily workouts. User can:
+- log multiple exercises in a workout on a given day.
+- track the name, type, weight, sets, reps, and duration of exercise.
+- track distance traveled if the exercise is a cardio exercise.
